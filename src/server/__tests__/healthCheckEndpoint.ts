7367b66283e8dbabcd16921b4495c0d9025cc68d
Alex Uribe <repzyyog@gmail.com>
2025-05-07 15:55:28 +0200

    Add test to healthCheck endpoint

---
+++ src/server/__tests__/healthCheckEndpoint.ts
@@ -0,0 +1,18 @@
+import request from "supertest";
+import app from "../app.js";
+
+describe("Given a GET / endpoint", () => {
+  describe("When it receives a request", () => {
+    test("Then it should respond with a 200 status code and a 'pong 🏓' message", async () => {
+      const expectedStatus = 200;
+      const expectedMessage = "pong 🏓";
+
+      const response = await request(app).get("/");
+
+      const body = response.body as { message: string };
+
+      expect(response.status).toBe(expectedStatus);
+      expect(body.message).toBe(expectedMessage);
+    });
+  });
+});
