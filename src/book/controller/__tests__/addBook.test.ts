be7100ebe5545a30fb9b8d112818134a6400dc7c
Alex Uribe <repzyyog@gmail.com>
2025-05-14 10:56:39 +0200

    Add tests to addBook method

---
+++ src/book/controller/__tests__/addBook.test.ts
@@ -0,0 +1,84 @@
+import { Model } from "mongoose";
+import {
+  dragonBallDataRead,
+  dragonBallVol1Read,
+} from "../../fixtures/fixtures.js";
+import { BookStructure } from "../../types.js";
+import { BookRequest, BookResponse } from "../types.js";
+import BookController from "../BookController.js";
+import { NextFunction } from "express";
+import statusCodes from "../../../globals/statusCodes.js";
+import ServerError from "../../../server/ServerError/ServerError.js";
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("Given the addBook method of BookController", () => {
+  const res: Pick<BookResponse, "status" | "json"> = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+
+  const next = jest.fn();
+
+  const req: Pick<BookRequest, "body"> = {
+    body: { book: dragonBallDataRead },
+  };
+  describe("When it receives Dragon Ball book data", () => {
+    const bookModel: Pick<Model<BookStructure>, "findOne" | "insertOne"> = {
+      findOne: jest.fn().mockReturnValue({
+        exec: jest.fn().mockResolvedValue(null),
+      }),
+      insertOne: jest.fn().mockResolvedValue(dragonBallVol1Read),
+    };
+
+    const bookController = new BookController(
+      bookModel as Model<BookStructure>,
+    );
+
+    test("Then it should call the response's method status with 201", async () => {
+      await bookController.addBook(
+        req as BookRequest,
+        res as BookResponse,
+        next as NextFunction,
+      );
+
+      expect(res.status).toHaveBeenCalledWith(statusCodes.CREATED);
+    });
+
+    test("Then it should call the response's method json with Dragon Ball book", async () => {
+      await bookController.addBook(
+        req as BookRequest,
+        res as BookResponse,
+        next as NextFunction,
+      );
+
+      expect(res.json).toHaveBeenCalledWith({ book: dragonBallVol1Read });
+    });
+  });
+
+  describe("When it receives the existent Dragon Ball book data", () => {
+    test("Then it should call the response's method status with a 409 and a 'Book already exists' message", async () => {
+      const bookModel: Pick<Model<BookStructure>, "findOne"> = {
+        findOne: jest.fn().mockReturnValue({
+          exec: jest.fn().mockResolvedValue(dragonBallVol1Read),
+        }),
+      };
+
+      const bookController = new BookController(
+        bookModel as Model<BookStructure>,
+      );
+
+      const error = new ServerError(409, "Book already exists");
+
+      await bookController.addBook(
+        req as BookRequest,
+        res as BookResponse,
+        next as NextFunction,
+      );
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+});
